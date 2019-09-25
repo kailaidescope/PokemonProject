787d16bca648156b0d7ca0e0d1970ad105f2f1c1
m3lodicaman <33330721+m3lodicaman@users.noreply.github.com>
2019-09-25 11:06:07 -0400

    Update1

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public AudioClip clip1;
+    public AudioClip clip2;
+    public AudioClip clip3;
+    public AudioClip clip4;
+
+    private AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = gameObject.GetComponent<AudioSource>();
+        source.clip = clip1;
+        source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            source.clip = clip1;
+            source.Play();
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            source.clip = clip2;
+            source.Play();
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            source.clip = clip3;
+            source.Play();
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            source.clip = clip4;
+            source.loop = false;
+            source.Play();
+        }
+    }
+}
