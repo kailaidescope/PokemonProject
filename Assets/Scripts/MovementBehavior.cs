d9ce0d52249e5ad7229354cb2df499dc10e0fa0d
m3lodicaman <33330721+m3lodicaman@users.noreply.github.com>
2019-09-25 17:37:21 -0400

    Added button to earth start

--- Assets/Scripts/MovementBehavior.cs
+++ Assets/Scripts/MovementBehavior.cs
@@ -5,28 +5,24 @@ using UnityEngine.UI;
 
 public class MovementBehavior : MonoBehaviour
 {
-
-    private bool grounded;
-    private float flyPower = 1;
-    public Image flyMeter;
-    private float meterMax;
-    private float meterMaxW;
-    private Camera cam;
     public float RotationSpeed;
+    public float YawSpeed;
     public float flySpeed;
+    public float jumpSpeed;
     public Transform center;
+
+    private Camera cam;
+    private bool grounded;
+
 
     private void Start()
     {
-        meterMaxW = 30f;
-        meterMax = 130f;
         cam = gameObject.GetComponentInChildren<Camera>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         grounded = true;
-        flyPower = 1;
     }
 
     private void OnCollisionExit(Collision collision)
@@ -38,16 +34,22 @@ public class MovementBehavior : MonoBehaviour
     void Update()
     {
 
-        flyMeter.rectTransform.sizeDelta = new Vector2(meterMaxW, meterMax * flyPower);
-
-        transform.Rotate(0f, Input.GetAxis("Mouse X") * RotationSpeed, 0f);
+       transform.Rotate(0f, Input.GetAxis("Mouse X") * RotationSpeed, 0f);
         if (grounded)
         {
-            center.Rotate(-Input.GetAxis("Mouse Y") * RotationSpeed, 0f, 0f);
+            center.Rotate(0f, -Input.GetAxis("Mouse Y") * RotationSpeed, 0f);
         }
         if (!grounded)
         {
             transform.Rotate(-Input.GetAxis("Mouse Y") * RotationSpeed, 0f, 0f);
+        }
+        if (Input.GetKey("q") && !grounded)
+        {
+            transform.Rotate(0f, 0f, YawSpeed);
+        }
+        if (Input.GetKey("e") && !grounded)
+        {
+            transform.Rotate(0f, 0f, -YawSpeed);
         }
         if (Input.GetKey("w"))
         {
@@ -57,7 +59,6 @@ public class MovementBehavior : MonoBehaviour
         {
             transform.parent = null;
             gameObject.GetComponent<Rigidbody>().AddForce(transform.localRotation * Vector3.forward * flySpeed, ForceMode.Impulse);
-            flyPower -= 0.05f;
         }
         if (Input.GetKey("s"))
         {
@@ -67,19 +68,35 @@ public class MovementBehavior : MonoBehaviour
         {
             transform.parent = null;
             gameObject.GetComponent<Rigidbody>().AddForce(transform.localRotation * Vector3.back * flySpeed, ForceMode.Impulse);
-            flyPower -= 0.05f;
         }
-        if (Input.GetKeyDown("space") && flyPower > 0)
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-0.1f, 0f, 0f);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(0.1f, 0f, 0f);
+        }
+        if (Input.GetKeyDown("space") && grounded)
         {
             transform.parent = null;
-            gameObject.GetComponent<Rigidbody>().AddForce(transform.localRotation * Vector3.up * 10f, ForceMode.Impulse);
-            flyPower -= 0.05f;
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.localRotation * Vector3.up * jumpSpeed, ForceMode.Impulse);
         }
-        if (Input.GetKeyDown("z") && flyPower > 0)
+        if (Input.GetKeyDown("space") && !grounded)
         {
             transform.parent = null;
-            gameObject.GetComponent<Rigidbody>().AddForce(transform.localRotation * Vector3.down * 10f, ForceMode.Impulse);
-            flyPower -= 0.05f;
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.localRotation * Vector3.up * jumpSpeed, ForceMode.Impulse);
+        }
+        if (Input.GetKeyDown("z") && grounded)
+        {
+            transform.parent = null;
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.localRotation * Vector3.down * jumpSpeed, ForceMode.Impulse);
+        }
+        
+        if (Input.GetKeyDown("z") && !grounded)
+        {
+            transform.parent = null;
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.localRotation * Vector3.down * flySpeed, ForceMode.Impulse);
         }
         if (Input.mouseScrollDelta.y > 0)
         {
