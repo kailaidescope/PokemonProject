ed5323473ae9af0f598280cc3bccb9a832af2fef
m3lodicaman <33330721+m3lodicaman@users.noreply.github.com>
2019-10-03 11:05:56 -0400

    Finished stuff

---
+++ Assets/Scripts/TimeDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeDisplay : MonoBehaviour
+{
+    private float time;
+    public UnityEngine.UI.Text text1;
+    public UnityEngine.UI.Text text2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = Timer.finalTime;
+        text1.text = "You finished in " + time + " seconds!";
+        text2.text = "You finished in " + time + " seconds!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    public UnityEngine.UI.Text text;
+    public static float time;
+    public static float finalTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0f;
+        text.text = "Time - " + time;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        time += Time.fixedDeltaTime;
+        text.text = "Time - " + time;
+    }
+
+    private void OnDestroy()
+    {
+        finalTime = time;
+    }
+}
